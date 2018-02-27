20d7689cf401feef1a9b06fb6f3643076aac6b34
Eric Fossum <fossum.eric@gmail.com>
2018-02-26 20:43:05 -0800

    Major improvement to install script

--- install.sh
+++ install.sh
@@ -1,19 +1,44 @@
-sudo su
+#!/usr/bin/bash
 
 # Check for git and git-lfs
 
 # Add miner user
-adduser --system --no-create-home --disabled-password miner
+id -u miner > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+  echo "Adding miner user for service"
+  sudo adduser --system --no-create-home --disabled-password miner
+fi
 
 # Get repo
 cd /opt
-git clone http://thefoss.net/ericfoss/ethminer.git
+if [ -d /opt/ethminer ]
+then
+  echo "Pulling latest Git repo changes"
+  cd ethminer
+  git pull
+else
+  echo "Cloning ethminer repo"
+  git clone http://thefoss.net/ericfoss/ethminer.git
+fi
 
 # Link service
 cd /opt/ethminer/
-ln -s /opt/ethminer/ethminer.service /etc/systemd/system/ethminer.service
+if [ -f /opt/systemd/system/ethminer ]
+then
+  echo "Removing existing ethminer service"
+  sudo systemctl stop ethminer
+  sudo systemctl disable ethminer
+fi
+echo "Creating systemd service file"
+sudo cp --remove-destination --force /opt/ethminer/ethminer.service /lib/systemd/system/
 
 # Start service
-systemctl daemon-reload
-systemctl enable ethminer
-systemctl start ethminer
+if [ -f /lib/systemd/system/ethminer.service ]
+then
+  echo "Enabling and starting the service"
+  sudo systemctl daemon-reload
+  sudo systemctl enable ethminer
+  sudo systemctl start ethminer
+  echo "You can now check the service with 'sudo systemctl status ethminer'"
+fi
